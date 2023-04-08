51f197e42e28219fda042e34149c340fec73a038
kymiller <kylemiller457@gmail.com>
2023-04-08 15:06:54 -0500

    Changing header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,7 +19,7 @@ const Home: NextPage = () => {
             <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
                 <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
                     <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-                        What is up <span className="text-[hsl(280,100%,70%)]">T3</span> App!!
+                        kylemiller.<span className="text-[hsl(280,100%,70%)]">io</span>
                     </h1>
                     <div className="flex flex-col items-center gap-2">
                         <p className="text-2xl text-white">
