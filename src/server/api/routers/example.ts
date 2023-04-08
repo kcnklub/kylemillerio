4f4bdc74a85ca7901822801566a330930422fb56
kymiller <kylemiller457@gmail.com>
2023-04-08 12:32:46 -0500

    init commit

---
+++ src/server/api/routers/example.ts
@@ -0,0 +1,35 @@
+import { z } from "zod";
+
+import {
+    createTRPCRouter,
+    publicProcedure,
+    protectedProcedure,
+} from "~/server/api/trpc";
+
+export const exampleRouter = createTRPCRouter({
+    hello: publicProcedure
+        .input(z.object({ text: z.string() }))
+        .query(({ input }) => {
+            return {
+                greeting: `Hello ${input.text}`,
+            };
+        }),
+
+    getAll: publicProcedure.query(({ ctx }) => {
+        return ctx.prisma.example.findMany();
+    }),
+
+    getSecretMessage: protectedProcedure.query(() => {
+        return "you can now see this secret message!";
+    }),
+});
+
+export const anotherRouter = createTRPCRouter({
+    helloworld: publicProcedure
+        .input(z.object({ text: z.string() }))
+        .query(({ input }) => {
+            return {
+                greeting: `Whatever this is cool! ${input.text}`
+            }
+        }),
+})
